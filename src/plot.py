5a8fbde0dbc8f8ff6e75a042cf1057840b0e2580
Marco Schouten <marscho@dtu.dk>
2025-01-08 17:34:41 +0100

    ASDASD

---
+++ src/plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# Simple plot
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
+plt.title("Simple Matplotlib Plot")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+
+# Show the plot
+plt.show()
