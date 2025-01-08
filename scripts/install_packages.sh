a06ea8f0c348ddf751903476f4d28db674c5dc64
Marco Schouten <marscho@dtu.dk>
2025-01-08 17:31:52 +0100

    SAM WORKS FUCK YEAH

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -7,17 +7,4 @@ pip3 install torch torchvision torchaudio --index-url https://download.pytorch.o
 pip install --upgrade transformers>=4.37.0
 
 # Install additional packages
-pip install notebook ipywidgets platformdirs opencv-python qwen-vl-utils matplotlib
-
-git clone https://github.com/facebookresearch/sam2.git
-
-cd sam2
-
-pip install -e ".[notebooks]"
-
-
-
-#### IT FAILED
-# pip install 'git+https://github.com/facebookresearch/sam2.git'
-
-# wget https://dl.fbaipublicfiles.com/segment_anything_2/092824/sam2.1_hiera_large.pt
+pip install notebook ipywidgets opencv-python qwen-vl-utils matplotlib ultralytics platformdirs
