721a9d793db158ea640c66c3bbbf625ac63bb1b8
Marco Schouten <marscho@dtu.dk>
2025-02-03 10:59:23 +0100

    test

---
+++ run_job.sh
@@ -0,0 +1,33 @@
+source .venv2/bin/activate
+
+module load cuda
+
+# Define the cache path
+CACHE_PATH="/dtu/blackhole/14/189044/marscho/cache/"
+
+# Ensure the directory exists
+mkdir -p "$CACHE_PATH/tmp"
+
+# Export environment variables
+export HF_HOME="$CACHE_PATH"
+export TRANSFORMERS_CACHE="$CACHE_PATH"
+export DATASETS_CACHE="$CACHE_PATH"
+export TORCH_HOME="$CACHE_PATH"
+export TORCH_CACHEDIR="$CACHE_PATH"
+export CUDA_CACHE_PATH="$CACHE_PATH"
+export WANDB_CACHE_DIR="$CACHE_PATH"
+export WANDB_DATA_DIR="$CACHE_PATH"
+export PYTHONPYCACHEPREFIX="$CACHE_PATH"
+export TMPDIR="$CACHE_PATH/tmp"
+
+# for easier to read terminal errors
+export BETTER_EXCEPTIONS=1
+
+python src/main_benchmark.py \
+    --in_dir benchmark_results/input_benchmark \
+    --out_dir benchmark_results/output_benchmark \
+    --edit "grayscale" \
+    --max_objects 5 \
+    --dataset_size_samples 100 \
+    --reasoning \
+    --draw
