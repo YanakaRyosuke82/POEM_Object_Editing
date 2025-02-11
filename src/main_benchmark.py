ab8ca41a27772dad64a208ecb07c02c3c1b93eae
Marco Schouten <marscho@dtu.dk>
2025-02-11 14:23:21 +0100

    refactor and added downlaoded download_dataset

--- src/main_benchmark.py
+++ src/main_benchmark.py
@@ -158,6 +158,8 @@ def main():
 
         for sample in dataset:
             input_image, user_promppt, save_file_name = sample["input_image"], sample["edit_prompt"], sample["id"]
+            input_mask = sample["input_mask"]
+
             subfolder_name = save_file_name
             # Create input subfolder for this sample
             sample_input_dir = os.path.join(args.in_dir, subfolder_name)
@@ -167,6 +169,7 @@ def main():
             with open(edit_instruction_file, "w") as file:
                 file.write(user_promppt)
             input_image.save(input_path)
+            input_mask.save(os.path.join(args.in_dir, subfolder_name, "input_mask.png"))
             # save the savefilename to a txt file
             with open(os.path.join(args.in_dir, subfolder_name, "save_file_name.txt"), "w") as file:
                 file.write(save_file_name)
